f8373b90187f9009942f9c6ae18b91461a70eb1d
patel yash <yp1679149@gmail.com>
2025-02-20 18:12:51 -0500

    Update SQLQuery2.sql

--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -30,7 +30,7 @@ case when Age between 18 and 24 then 'A1'
 Else case when Age between 25 and 30 then 'A2'
 else 'A3' end end 
 
-//columan distribution 
+//column distribution 
 
 select * from INFORMATION_SCHEMA.COLUMNS where table_name like 'Dpression student Dataset'
 
@@ -61,6 +61,42 @@ group by Family_History_of_Mental_Illness
 
 select Depression, count(*) from [dbo].[Depression+Student+Dataset]
 group by Depression
+
+// add index_col 
+
+ALTER TABLE [dbo].[Depression+Student+Dataset]
+ADD Index_col int identity(1,1)
+
+select * from [dbo].[Depression+Student+Dataset]
+
+
+
+// Type converstion error
+UPDATE [dbo].[Depression+Student+Dataset]
+SET Depression = 'Yes' WHERE Depression = 1;
+
+//check schema
+
+SELECT * FROM INFORMATION_SCHEMA.columns WHERE TABLE_NAME LIKE 'Depression+Student+Dataset'
+
+
+//change datatype of Depression col
+
+ALTER table [dbo].[Depression+Student+Dataset]
+ALTER COLUMN Depression VARCHAR(MAX)
+
+//Update depression col values
+
+UPDATE [dbo].[Depression+Student+Dataset]
+SET Depression = 'Yes' WHERE Depression = '1';
+
+UPDATE [dbo].[Depression+Student+Dataset]
+SET Depression = 'No' WHERE Depression = '0';
+
+//observe updated value
+
+SELECT Depression, COUNT(*) FROM [dbo].[Depression+Student+Dataset] GROUP BY Depression
+
 
 
 
